00e2755eb4ab446c54ba6ddc7dd2ad5db81a910d
Noah Gambee <noah@gambee.dev>
2020-02-04 12:55:20 -0700

    Remame data properties and remove console logs

--- src/App/App.js
+++ src/App/App.js
@@ -6,13 +6,13 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: ''
+      name: '',
+      areas: [],
     };
   }
   componentDidMount() {
     fetch('http://localhost:3001/api/v1/areas')
       .then(response => response.json())
-      // .then(areasData => console.log(areasData))
       .then(areasData => {
         const promises = areasData.areas.map(area => {
           return fetch(`http://localhost:3001${area.details}`)
@@ -25,8 +25,6 @@ export default class App extends Component {
               }
             })
         })
-        // console.log(promises);
-        console.log(Promise.all(promises));
         return Promise.all(promises)
       })
       .then(areas => this.setState({ areas }))
