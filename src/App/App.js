60f03e8b677c3b0d369df06a5f669d0cceafa4bf
Noah Gambee <noah@gambee.dev>
2020-02-04 12:28:40 -0700

    Add fetch() request and first .then statement

--- src/App/App.js
+++ src/App/App.js
@@ -10,8 +10,16 @@ export default class App extends Component {
     };
   }
   componentDidMount() {
-    
+    fetch('http://localhost:3001/api/v1/areas')
+      .then(response => response.json())
+      // .then(areasData => console.log(areasData))
+      .then(areasData => {
+        const promises = areasData.areas.map(area => {
+          // return fetch(area.details)
 
+          console.log(`localhost:3000${area.details}`);
+        })
+      })
   }
 
   updateUser = userName => {
