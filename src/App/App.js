46368f7a6c7fea6c0145cf30b192ddb7b671feb3
Noah Gambee <noah@gambee.dev>
2020-02-04 12:37:00 -0700

    Update promises varibale to return object with needed properties

--- src/App/App.js
+++ src/App/App.js
@@ -17,6 +17,13 @@ export default class App extends Component {
         const promises = areasData.areas.map(area => {
           return fetch(`http://localhost:3001${area.details}`)
             .then(response => response.json())
+            .then(info => {
+              return {
+                shortName: area.area,
+                name: info.name,
+                description: info.about,
+              }
+            })
         })
         console.log(promises);
       })
