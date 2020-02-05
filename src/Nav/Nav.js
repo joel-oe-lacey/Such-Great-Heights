551d6d1ce88c1842deaab93472f5ec5dd2e630e3
Noah Gambee <noah@gambee.dev>
2020-02-05 16:26:01 -0700

    Add button styling to area cards

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = ({ userName }) => {
   return (
     <section className='nav-bar'>
       <img className='logo-nav'></img>
-      <h2 className='username'>Welcome, {userName}!</h2>
+      <h2 className='username'>welcome, {userName}!</h2>
       <button className='view-areas-btn'>View Areas</button>
       <button className='log-out-btn'>Log Out</button>
     </section>
