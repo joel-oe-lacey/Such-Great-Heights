7dc999095c36d825538f1493fe9abc9a71accf64
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-05 20:55:21 -0700

    FIx arrow bug

--- src/Carousel/Arrow/Arrow.js
+++ src/Carousel/Arrow/Arrow.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Arrow = ({direction, changeImg}) => {
     return <button className='arrow-btn' onClick={() => changeImg(direction)}>
-            {direction === 'left' ? '&#x2B05;' : '&#x27A1;'}
+        {direction === 'left' ? '←' : '→'}
         </button>
 }
 
