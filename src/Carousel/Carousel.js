10f327bb1704f40dc0076a0fda52fab543482f20
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-05 20:55:09 -0700

    Refactor Carousel to handle prop error

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -3,8 +3,8 @@ import './Carousel.scss';
 import Arrow from './Arrow/Arrow';
 
 export default class Carousel extends Component {
-    constructor({ id, name, imgTags }) {
-        super({ id, name, imgTags });
+    constructor(props) {
+        super(props)
         this.state = {
             imageIndex: 0,
         }
@@ -15,9 +15,9 @@ export default class Carousel extends Component {
         let newIndex;
 
         if (direction === 'left') {
-            newIndex = imageIndex === 0 ? 2 : imageIndex--;
+            newIndex = imageIndex === 0 ? 2 : imageIndex - 1;
         } else {
-            newIndex = imageIndex === 2 ? 0 : imageIndex++;
+            newIndex = imageIndex === 2 ? 0 : imageIndex + 1;
         }
         
         this.setState({
@@ -27,7 +27,7 @@ export default class Carousel extends Component {
 
     render() {
         return <section className="carousel">
-            <img src={`${this.id}_${this.imgTags[this.state.imageIndex]}.jpg`} alt={this.name}/>
+            <img src={`../../images/${this.props.id}_${this.props.imgTags[this.state.imageIndex]}.jpg`} alt={this.props.name}/>
             <Arrow direction="left" changeImg={this.changeImg} />
             <Arrow direction="right" changeImg={this.changeImg} />
         </section>
