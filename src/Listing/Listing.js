256bdfa152fd7f768fd24efdc3ea867e9f32ea60
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-07 17:24:47 -0700

    Refactor listing component to handle link

--- src/Listing/Listing.js
+++ src/Listing/Listing.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Listing.scss'
+import { Link } from "react-router-dom";
 
-const Listing = ({ id, name, viewListingInfo }) => {
+const Listing = ({ id, name, selectListing }) => {
     return (
-        <section className='listing'>
-            <h3>{name}</h3>
-            <img src={`../../images/${id}_a.jpg`} alt={name}/>
-            <button onClick={() => viewListingInfo(id)}>View Details</button>
-        </section>
-    )
+      <section className="listing">
+        <h3>{name}</h3>
+        <img src={`../../images/${id}_a.jpg`} alt={name} />
+        <Link to={`/details`}>
+          <button onClick={() => selectListing(id)}>View Details</button>
+        </Link>
+      </section>
+    );
 }
 
 export default Listing;
