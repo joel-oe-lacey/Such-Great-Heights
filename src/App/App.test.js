bc2b3a5fe9d662bac94d8f5b6b9808ad23c79e59
Noah Gambee <noah@gambee.dev>
2020-02-11 08:15:08 -0700

    Add App method tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,58 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App/App';
+import App from './App.js';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe ('App', ()=> {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+    });
+
+  it('should add a favorite to state when addFav() is run', () => {
+    let mockListing = {id: 123, name: 'cool looking house'}
+    let defaultState = {
+      areas: [],
+      favorites: [],
+      listings:[],
+      name: "user1",
+      tripType: "vacation"
+    }
+    let updatedState = {
+      areas: [],
+      favorites: [mockListing],
+      listings:[],
+      name: "user1",
+      tripType: "vacation"
+    }
+    wrapper.instance().setState(defaultState)
+    wrapper.instance().addFavorite(mockListing)
+    expect(wrapper.state()).toEqual(updatedState)
+  })
+
+  it('should remove a favorite from state when removeFav is called', () => {
+    let mockListing = {id: 123, name: 'cool looking house'}
+    let defaultState = {
+      areas: [],
+      favorites: [mockListing]
+    }
+    let updatedState = {
+      areas: [],
+      favorites: [],
+      listings:[],
+      name: "user1",
+      tripType: "vacation",
+    }
+    wrapper.instance().setState(defaultState)
+    wrapper.instance().removeFavorite(mockListing);
+    expect(wrapper.state()).toEqual(updatedState);
+  })
+  
+  it('should update state.name when updateUser is invoked', () => {
+    wrapper.instance().setState({});
+    wrapper.instance().updateUser('user123');
+    expect(wrapper.state()).toEqual({areas: [], favorites: [], listings: [], name: 'user123', tripType: 'vacation'})
+  })
+})
