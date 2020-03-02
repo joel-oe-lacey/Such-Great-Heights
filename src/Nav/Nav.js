e4b896fd4f11a1fba166d5481823913071e663f9
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-01 21:11:49 -0700

    Update basic element styling

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -8,8 +8,8 @@ import PropTypes from 'prop-types';
 const Nav = ({ userName }) => {
   return (
     <section className='nav-bar'>
-      <img className='logo-nav'></img>
-      <h2 className='username'>{`welcome, ${userName}!`}</h2>
+      <img className='logo-nav' src="/../../Images/mountain.svg" alt="A lofty mountain"></img>
+      <h2 className='username'>{`Welcome, ${userName}!`}</h2>
       <NavLink to='/favorites'><button className='view-favorites-btn'>View Favorites</button></NavLink>
       <NavLink to='/areas'><button className='view-areas-btn'>View Areas</button></NavLink>
       <NavLink to='/'><button className='log-out-btn'>Log Out</button></NavLink>
